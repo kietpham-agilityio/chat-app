dc20517f8e4e3cecde37939a9df08e9bd796622d
Kiet Pham <kiet.pham@asnet.com.vn>
2025-05-21 17:37:25 +0700

    setup theme

---
+++ lib/core/themes/app_palette.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CAPalette {
+  static const Color primaryBlue = Color(0xFF3355FF);
+  static const Color error = Color(0xFFE61A1A);
+  static const Color genericWhite = Color(0xFFFFFFFF);
+  static const Color genericBlack = Color(0xFF000000);
+
+  static const MaterialColor grey = MaterialColor(0xFF7F878D, <int, Color>{
+    1: Color(0xFFF2F2F2),
+    2: Color(0xFFE5E5E5),
+    3: Color(0xFFB3B3B3),
+    4: Color(0xFF666666),
+    5: Color(0xFF333333),
+    6: Color(0xFF1A1A1A),
+  });
+}
---
+++ lib/core/themes/typography.dart
@@ -0,0 +1,27 @@
+abstract class CATypography {
+  static const String fontSFProText = 'SFProText';
+
+  /// Size Headline Small
+  static const double fontSizeHeadlineSmall = 24;
+  static const double heightHeadlineSmall = 1.33;
+
+  /// Size Title Large
+  static const double fontSizeTitleLarge = 20;
+  static const double heightTitleLarge = 1.4;
+
+  /// Size Title Medium
+  static const double fontSizeTitleMedium = 16;
+  static const double heightTitleMedium = 1.5;
+
+  /// Size Body Large
+  static const double fontSizeBodyLarge = 16;
+  static const double heightBodyLarge = 1.5;
+
+  /// Size Body Medium
+  static const double fontSizeBodyMedium = 14;
+  static const double heightBodyMedium = 1.42;
+
+  /// Size Body Small
+  static const double fontSizeBodySmall = 12;
+  static const double heightBodySmall = 1.33;
+}
