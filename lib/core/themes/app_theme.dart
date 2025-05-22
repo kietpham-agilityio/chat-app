c2b73d471d3f78ffa3b83a158a29766aeb6dc939
Kiet Pham <141294339+kietpham-agilityio@users.noreply.github.com>
2025-05-22 16:55:36 +0700

    Feat/create common widget text (#37)
    * create common widget text

--- lib/core/themes/app_theme.dart
+++ lib/core/themes/app_theme.dart
@@ -14,8 +14,56 @@ class CATheme {
 
       /// Icon Theme
       iconTheme: _iconTheme,
+
+      // / Text Theme
+      textTheme: _textTheme,
     );
   }
 
   static final _iconTheme = IconThemeData(size: 24, color: CAPalette.grey[6]);
+
+  static final _textTheme = TextTheme(
+    headlineSmall: TextStyle(
+      fontFamily: CATypography.fontSFProText,
+      fontSize: CATypography.fontSizeTitleLarge,
+      fontWeight: FontWeight.w600,
+      height: CATypography.heightHeadlineSmall,
+      color: CAPalette.genericBlack,
+    ),
+    titleLarge: TextStyle(
+      fontFamily: CATypography.fontSFProText,
+      fontSize: CATypography.fontSizeTitleLarge,
+      fontWeight: FontWeight.w600,
+      height: CATypography.heightTitleLarge,
+      color: CAPalette.genericBlack,
+    ),
+    titleMedium: TextStyle(
+      fontFamily: CATypography.fontSFProText,
+      fontSize: CATypography.fontSizeTitleMedium,
+      fontWeight: FontWeight.w600,
+      height: CATypography.heightTitleMedium,
+      color: CAPalette.genericBlack,
+    ),
+    bodyLarge: TextStyle(
+      fontFamily: CATypography.fontSFProText,
+      fontSize: CATypography.fontSizeBodyLarge,
+      fontWeight: FontWeight.w400,
+      height: CATypography.heightBodyLarge,
+      color: CAPalette.genericBlack,
+    ),
+    bodyMedium: TextStyle(
+      fontFamily: CATypography.fontSFProText,
+      fontSize: CATypography.fontSizeBodyMedium,
+      fontWeight: FontWeight.w400,
+      height: CATypography.heightBodyMedium,
+      color: CAPalette.genericBlack,
+    ),
+    bodySmall: TextStyle(
+      fontFamily: CATypography.fontSFProText,
+      fontSize: CATypography.fontSizeBodySmall,
+      fontWeight: FontWeight.w400,
+      height: CATypography.heightBodySmall,
+      color: CAPalette.genericBlack,
+    ),
+  );
 }
