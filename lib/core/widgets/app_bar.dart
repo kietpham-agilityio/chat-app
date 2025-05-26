07fb19aa727503b2339fc0e59f6582328cace579
Kiet Pham <kiet.pham@asnet.com.vn>
2025-05-26 10:15:49 +0700

    create common widget app bar

---
+++ lib/core/widgets/app_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class CAAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CAAppBar({
+    this.title = const SizedBox.shrink(),
+    this.toolbarHeight = 44,
+    this.leading,
+    this.trailing,
+    super.key,
+  });
+
+  /// The widget to be displayed in the center of the app bar.
+  ///
+  /// The [title] property is optional and can be any widget.
+  final Widget? title;
+
+  /// The height of the app bar.
+  ///
+  /// The [toolbarHeight] property is optional and can be used to customize the
+  /// height of the app bar. The default height is 44.
+  final double toolbarHeight;
+
+  /// The widget to be displayed on the left side of the app bar.
+  ///
+  /// The [leading] property is optional and can be any widget.
+  final Widget? leading;
+
+  /// A list of widgets to be displayed on the right side of the app bar.
+  ///
+  /// The [trailing] property is optional and can be a list of widgets. They are
+  /// displayed on the right side of the app bar.
+  final List<Widget>? trailing;
+
+  @override
+  Size get preferredSize => Size.fromHeight(toolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(toolbarHeight),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AppBar(
+            toolbarHeight: toolbarHeight,
+            leading: leading,
+            actions: [if (trailing != null) ...trailing!],
+            title: title,
+          ),
+          Divider(height: 0),
+        ],
+      ),
+    );
+  }
+}
