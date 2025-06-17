43606651a6be00a2dd1eab1c68116f6b90e695cd
Kiet Pham <kiet.pham@asnet.com.vn>
2025-06-17 10:24:41 +0700

    remove unused code

--- lib/screens/auth/states/auth_state.dart
+++ lib/screens/auth/states/auth_state.dart
@@ -2,43 +2,6 @@ part of 'auth_bloc.dart';
 
 enum AuthStatus { authenticated, unauthenticated }
 
-// final class AuthState extends Equatable {
-//   const AuthState({UserModel? user, String? fcmToken})
-//     : this._(user: user, fcmToken: fcmToken);
-
-//   const AuthState._({this.user = UserModel.empty, this.fcmToken});
-
-//   final String? fcmToken;
-//   final UserModel? user;
-
-//   @override
-//   List<Object?> get props => [user, fcmToken];
-// }
-
-// final class AuthState extends Equatable {
-//   const AuthState({UserModel user = UserModel.empty, String? fcmToken})
-//     : this._(
-//         status: user == UserModel.empty
-//             ? AppStatus.unauthenticated
-//             : AppStatus.authenticated,
-//         user: user,
-//         fcmToken: fcmToken,
-//       );
-
-//   const AuthState._({
-//     required this.status,
-//     this.user = UserModel.empty,
-//     this.fcmToken,
-//   });
-
-//   final AppStatus status;
-//   final UserModel user;
-//   final String? fcmToken;
-
-//   @override
-//   List<Object> get props => [status, user];
-// }
-
 final class AuthState extends Equatable {
   const AuthState({
     UserModel? user,
